2a0cc9c8ac9b5b8ad171afa38f3b57ea1966e9b2
dungpham2502 <101266728+dungpham2502@users.noreply.github.com>
2022-10-09 00:06:11 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+function Project1() {
+    window.location.href = "project1.html"
+}
+
+function Project2() {
+    window.location.href = "project2.html"
+}
+
+function Project3() {
+    window.location.href = "project3.html"
+}
+
+function Project4() {
+    window.location.href = "project4.html"
+}
+
+function Project5() {
+    window.location.href = "project5.html"
+}
+
+function Project6() {
+    window.location.href = "project6.html"
+}
+
+function Project7() {
+    window.location.href = "project7.html"
+}
+
+function Project8() {
+    window.location.href = "project8.html"
+}
+
+function Project9() {
+    window.location.href = "project9.html"
+}
